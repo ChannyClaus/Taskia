3bb70d5a442f39a597405722874a461ec826de85
chan <chankang17@gmail.com>
2023-05-18 15:03:33 -0400

    onClick css seems correct

--- components/CardService.js
+++ components/CardService.js
@@ -1,17 +1,19 @@
 import Image from 'next/image';
 
 const CardService = ({ id, icon, title, description, template, setTemplate }) => {
+    const selected = template === id
+
     return (
-        <div className="card" id={id}>
+        <div className="card" style={selected ? { 'background-color': '#59cea7' } : {}} id={id}>
             <h3>{id}</h3>
             <Image src={icon} alt="Service-icon" width={200} height={200} />
-            <div className="preview">
+            <div className="preview" style={selected ? { 'background-color': 'white' } : {}}>
                 <a href="http://google.com">
                     Preview
                 </a>
             </div>
-            <div className='select' onClick={() => { setTemplate(id) }}>
-                <p>{template === id ? "Selected!" : "Select"}</p>
+            <div className='select' style={selected ? { 'background-color': 'black' } : {}} onClick={() => { setTemplate(id) }}>
+                <p>{selected ? "Selected!" : "Select"}</p>
             </div>
         </div>
     )
