ddbc4368fb6a08ac537ac780e4a0cf1a78bdc488
chan <chankang17@gmail.com>
2023-05-18 13:43:05 -0400

    css fiddling

--- components/CardService.js
+++ components/CardService.js
@@ -5,10 +5,12 @@ const CardService = ({ id, icon, title, description }) => {
         <div className="card" id={id}>
             <h3>{id}</h3>
             <Image src={icon} alt="Service-icon" width={200} height={200} />
-            <div className="info">
+            <div className="preview">
                 <a href="http://google.com">
                     Preview
                 </a>
+            </div>
+            <div className='select'>
                 <p>Select</p>
             </div>
         </div>
